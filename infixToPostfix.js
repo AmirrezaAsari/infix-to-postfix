050f736c8176c62c37e23add758a6fc3f24c03b7
unknown <amirreza.asari2@gmail.com>
2023-12-19 16:40:48 +0330

    calculate basic operations

--- infixToPostfix.js
+++ infixToPostfix.js
@@ -1,10 +1,12 @@
 function infixToPostfix(infix){
     const operators = {
-        "+" : 1,
-        "-" : 1,
-        "*" : 2,
-        "/" : 2,
-        "^" : 3
+        "+"   : 1,
+        "-"   : 1,
+        "*"   : 2,
+        "/"   : 2,
+        "^"   : 3,
+        'sin' : 4,
+        'cos' : 4
     };
 
     const stack = [];
@@ -15,11 +17,11 @@ function infixToPostfix(infix){
         let token = infix[i];
 
         if(/[a-zA-z0-9]/.test(token)){
-            output += token;
+            output += token + " ";
         }
         else if(token in operators){
             while(stack.length && operators[token] <= operators[stack[stack.length - 1]]){
-                output += stack.pop();
+                output += stack.pop() + " ";
             }
             stack.push(token);
         }
@@ -28,18 +30,55 @@ function infixToPostfix(infix){
         }
         else if(token === ")"){
             while(stack.length && stack[stack.length - 1] !== "("){
-                output += stack.pop();
+                output += stack.pop() + " ";
             }
             stack.pop(); //remove (
         }
     }
         while(stack.length){
-            output += stack.pop();
+            output += stack.pop() + " ";
         }
         return output;
 
 }
 
 
-const infix = "a + b * (c ^ d - e) / f";
+function calculate(postfix){
+    let stack = [];
+    postfix = postfix.replace(/\s/g, '');
+    for(let i = 0; i < postfix.length; i++){
+        let token = postfix[i];
+        if(! isNaN(parseInt(token))){
+            stack.push(parseInt(token));
+        }
+        else{
+            let val1 = stack.pop();
+            let val2 = stack.pop();
+
+            switch(token){
+                case "+" :
+                    stack.push(val2 + val1);
+                    break;
+                case "-" :
+                    stack.push(val2 - val1);
+                    break;
+                case "*" :
+                    stack.push(val2 * val1);
+                    break;
+                case "/" :
+                    stack.push(val2 / val1);
+                    break;
+                case "^" :
+                    stack.push(Math.pow(val2, val1));
+                    break;
+            }
+        }
+    }
+    return stack.pop();
+}
+
+
+const infix = "2 + 5 * 6 + 2";
 console.log(infixToPostfix(infix));
+console.log(calculate(infixToPostfix(infix)));
+
